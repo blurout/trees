c387d2048d817ace6b776511219392635be78979
Fatih <fatihsen539@gmail.com>
2022-02-14 22:55:02 -0800

    Update binaryTree.go

--- GO/binaryTree.go
+++ GO/binaryTree.go
@@ -1,14 +1,66 @@
 package main
 
-type treeNode struct {
-	val int
-	left *treeNode
-	right *treeNode
+import ( 
+	"fmt"
+	"math/rand"
+	"time"
+)
+type node struct {
+	val   int
+	left  *node
+	right *node
 }
 
 func main() {
-	root := treeNode{val: 9, left: nil, right: nil}
-	leftChild := treeNode{val: 6, left: nil, right: nil}
-	rightChild := treeNode{val: 12, left: nil, right: nil}
-	root.left, root.right = &leftChild, &rightChild
+	root := &node{val: 50}
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	root.Insert(Randomint())
+	fmt.Print(root.Search(15))
+}
+
+func (n *node) Insert (data int) {
+	if n.val < data {
+		if n.right == nil {
+			n.right = &node{val: data}
+		} else {
+			n.right.Insert(data)
+		}
+	} else if n.val > data {
+		if n.left == nil {
+			n.left = &node{val: data}
+		} else {
+			n.left.Insert(data)
+		}
+	}
+}
+
+func (n *node) Search (data int) bool {
+	if n == nil {
+		return false
+	}
+	if n.val < data {
+		return n.right.Search(data)
+	} else if n.val > data {
+		return n.left.Search(data)
+
+	}
+	return true
+}
+
+func Randomint() int {
+	rand.Seed(time.Now().UnixNano())
+	num := 0
+	num += rand.Intn(100)
+	return num
 }
